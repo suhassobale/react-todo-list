5d3c49e27c5a34f0007adee10fa43a88bff4810a
suhassobale <suhassobale@gmail.com>
2019-12-22 22:10:08 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/pages/About';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: 'dinner with wife',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'shopping in mall',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'walk in garden',
+        completed: true
+      }
+    ]
+  };
+  markComplete = id => {
+    console.log(id);
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+  delTodo = id => {
+    console.log('delete');
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+  AddTodo = title => {
+    console.log(title);
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+  render() {
+    console.log(this.state.todos);
+    return (
+      <Router>
+        <Header />
+        <Route
+          path="/"
+          exact
+          render={props => (
+            <React.Fragment>
+              <AddTodo AddTodo={this.AddTodo} />
+              <Todos
+                todos={this.state.todos}
+                markComplete={this.markComplete}
+                delTodo={this.delTodo}
+              />
+            </React.Fragment>
+          )}
+        />
+        <Route path="/about" component={About} />
+      </Router>
+    );
+  }
 }
 
 export default App;
